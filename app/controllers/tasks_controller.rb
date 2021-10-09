ec2059d039ac519789bafa6268e47083c74a3ac4
bananakiu <leandrenoelkiu@gmail.com>
2021-10-09 13:08:49 +0800

    add edit task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,16 @@
 class TasksController < ApplicationController
+    before_action :set_category
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+    def index
+        redirect_to @category
+    end
+    
     def new
-        @category = Category.find(params[:category_id])
         @task = @category.tasks.new
     end
     
     def create
-        @category = Category.find(params[:category_id])
         @task = @category.tasks.new(task_params)
         if @task.save
             redirect_to category_path(@category)
@@ -14,14 +19,32 @@ class TasksController < ApplicationController
         end
     end
 
+    def edit
+    end
+
+    def update
+        if @task.update(task_params)
+            # redirect_to @task
+            redirect_to category_path(@category)
+        else
+            render :edit
+        end
+    end
+
     def destroy
-        @category = Category.find(params[:category_id])
-        @task = @category.tasks.find(params[:id])
         @task.destroy
         redirect_to category_path(@category)
     end
     
     private
+        def set_category
+            @category = Category.find(params[:category_id])
+        end
+
+        def set_task
+            @task = @category.tasks.find(params[:id])
+        end
+
         def task_params
             params.require(:task).permit(:name, :notes, :date, :complete)
         end
