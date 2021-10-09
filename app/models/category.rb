9a88c355288e723747a3f15a9178e7b9de4bf32c
bananakiu <leandrenoelkiu@gmail.com>
2021-10-09 11:37:53 +0800

    setup tasks controller, views, and model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+    has_many :tasks
+
     validates :title, presence: true
+    # add max length
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true
+  # add maximum length validation for name & description
 end
