b9ea998db3017bb68d6e3005e8a7d20280a212db
bananakiu <leandrenoelkiu@gmail.com>
2021-10-18 05:33:05 +0800

    change validation max length for category title to 20 characters

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :tasks, dependent: :destroy
 
-    validates :title, presence: true, length: { maximum: 30 }
+    validates :title, presence: true, length: { maximum: 20 }
 end
