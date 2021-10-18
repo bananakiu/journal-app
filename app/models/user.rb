7638b65b24f4d99bf0bc1829c62821442fa28ead
bananakiu <leandrenoelkiu@gmail.com>
2021-10-18 18:10:48 +0800

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :categories
+
+  validates :email, presence: true, length: { maximum: 100 }
 end
