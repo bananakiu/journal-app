176cfa5feedf8a3a8edf523d7ae52917dd8fef93
bananakiu <leandrenoelkiu@gmail.com>
2021-10-18 19:57:13 +0800

    add model tests for category and task models

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # setup
+  setup do
+    @user = users(:one)
+  end
+
+  test "should not save Category without title" do
+    @category = @user.categories.create(title: nil)
+    assert_not @category.save, "saved category without title"
+  end
+  
+  test "should not save Category with title longer than 20 characters" do
+    @category = @user.categories.create(title: "a"*21)
+    assert_not @category.save, "saved category with title longer than 20 characters"
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # setup
+  setup do
+    @user = users(:one)
+  end
+
+  test "should not save task without name" do
+    @category = @user.categories.create(title: "Test Category")
+    task = @category.tasks.create(name: nil)
+    assert_not task.save, "saved category without name"
+  end
+
+  test "should not save task with name longer than 100 characters" do
+    @category = @user.categories.create(title: "Test Category")
+    task = @category.tasks.create(name: "a"*101)
+    assert_not task.save, "saved category with name longer than 100 characters"
+  end
+
+  test "should not save task with notes longer than 2000 characters" do
+    @category = @user.categories.create(title: "Test Category")
+    task = @category.tasks.create(name: "Test Task", notes: "a"*2001)
+    assert_not task.save, "saved category with notes longer than 2000 characters"
+  end
+
 end
