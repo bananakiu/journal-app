71bec603d41aeee537981a7f6ff18b8941ec4bcd
bananakiu <leandrenoelkiu@gmail.com>
2021-10-22 20:32:10 +0800

    add validation: category name must be unique (scoped per user_id

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,5 +3,5 @@ class Category < ApplicationRecord
     has_many :tasks, dependent: :destroy
 
     validates :title, presence: true, length: { maximum: 20 }
-    validates :title, uniqueness: true
+    validates :title, uniqueness: { scope: :user_id }
 end
