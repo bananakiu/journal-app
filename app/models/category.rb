b2a854abb6478a830021edc86beeb88603bac2d1
bananakiu <leandrenoelkiu@gmail.com>
2021-10-22 20:30:50 +0800

    add validation: category name must be unique

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ApplicationRecord
     has_many :tasks, dependent: :destroy
 
     validates :title, presence: true, length: { maximum: 20 }
+    validates :title, uniqueness: true
 end
